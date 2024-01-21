709c7b2f117072c02e40771158c7d22a82f74f41
Hossein <hossvadi@gmail.com>
2024-01-21 12:37:16 +0100

    data collection

---
+++ data_collection.py
@@ -0,0 +1,6 @@
+import yfinance as yf
+
+
+def fetch_stock_data(stock_symbol, start_date, end_date):
+    stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
+    return stock_data
